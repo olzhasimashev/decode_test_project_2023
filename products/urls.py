eaec8d0b28e71df65063a55377010b5353144dec
Imashev Olzhas <imashev.olzhas@gmail.com>
2023-07-05 22:43:31 +0600

    added third lesson

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, get_product
+from .views import index, get_product, get_category
 
 urlpatterns = [
-    path('', index , name = 'index'),
-    path('<pk>/', get_product),
+    path('products', index , name = 'index'),
+    path('products/<pk>/', get_product),
+    path('<pk>/', get_category),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, Category
 
 # Create your views here.
 
@@ -9,7 +9,11 @@ def index(request):
 
     return render(request,'products/index.html', {'products': p,})   
 
-def get_product(request,pk):
+def get_product(request, pk):
     p = Products.objects.get(pk=pk) # select * from products where pk = 2
 
     return render(request,'products/product.html', {'p': p,})
+
+def get_category(request, pk):
+    p = Products.objects.filter(category_id=pk)
+    return render(request,'products/category.html', {'products': p,})
