ed1fde4e5a0b5d14187aba0003afc7c26823b5ed
Imashev Olzhas <imashev.olzhas@gmail.com>
2023-07-15 14:56:34 +0600

    modified products/admin.py, ProductsAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Products, Category
 # Register your models here.
-admin.site.register(Products)
+# admin.site.register(Products)
 admin.site.register(Category)
+
+class ProductsAdmin (admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'date', 'category')
+    list_filter = ('price', 'date', 'category')
+    search_fields = ['name']
+
+admin.site.register(Products, ProductsAdmin)
+
+admin.site.site_header = "Aдмин панель о товарах"
+admin.site.site_title = "Aдмин панель сайта"
--- shop/urls.py
+++ shop/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('products.urls')),
+    # path('account', include('users.urls')),
 ]
 
 if settings.DEBUG:
